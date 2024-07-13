fd2b58f5d885b2a4070522eafc12539dc33c644a
aljithkj02 <kwdeathlok@gmail.com>
2024-07-13 11:47:53 +0530

    Add Ws server for google meet

---
+++ Projects/googleMeet/server/src/types/incoming.types.ts
@@ -0,0 +1,29 @@
+import { WebSocket } from "ws";
+
+export type AppStoreType = Record<string, SingleRoom>;
+
+interface SingleRoom {
+    owner: WebSocket;
+}
+
+export type GiveOfferPayload = {
+    roomId: string;
+    sdp: string;
+    userId: number;
+}
+
+export type GiveAnswerPayload = {
+    roomId: string;
+    sdp: string;
+    userId: number;
+}
+
+export type IceCandidateOwner = {
+    candidate: string;
+    roomId: string;
+}
+
+export type IceCandidateUser = {
+    candidate: string;
+    userId: number;
+}
