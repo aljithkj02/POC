ad09cf4888a354d1c527ffa7242160aef841fa8a
Aljith K J <kwdeathlok@gmail.com>
2024-09-25 21:05:06 +0530

    Add endpoints to send notifications

--- Frontend/fcm-server/src/index.js
+++ Frontend/fcm-server/src/index.js
@@ -2,10 +2,10 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import admin from "firebase-admin";
+import serviceAccount from "./serviceAccountKey.json" assert { type: 'json'};
 
+const tokenStore = new Set();
 dotenv.config();
-
-const serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
@@ -20,6 +20,35 @@ app.get('/', (req, res) => {
     res.json({
         status: true,
         message: "Welcome to my server!"
+    })
+})
+
+app.post('/save-token', (req, res) => {
+    tokenStore.add(req.body.token);
+    res.json({
+        status: true,
+        message: "Successfully added token"
+    })
+})
+
+app.get('/send', async (req, res) => {
+    const tokens = Array.from(tokenStore);
+
+    tokens.forEach(async (token) => {
+        const payload = {
+            notification: {
+              title: "This is the title",
+              body: "Hello buddy here your notification!",
+            },
+            token,
+        };
+
+       await admin.messaging().send(payload);
+    })
+
+    res.json({
+        status: true,
+        message: "Successfully send the notification!"
     })
 })
 
